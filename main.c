2a359e022289f1dce917c5bc69f665ec48d37b41
Le Juez Victor <90587919+Bigfoot71@users.noreply.github.com>
2022-08-27 16:09:09 +0200

    Update main.c

--- main.c
+++ main.c
@@ -6,45 +6,98 @@
 
 #include <SDL2/SDL.h>
 
+#define FPS 60
+#define TPF 1000/FPS
 #define WIN_W 640
 #define WIN_H 480
 
-/* TYPE/STRUCT FOR PROGRAM */
+/*
+    NOTE: To speed up this program it would surely be necessary to do the work in a texture and not directly on the renderer.
+*/
 
-typedef struct _mandala_pix mandala_pix;
+/* Typedefs */
 
-struct _mandala_pix {
-    double x, y,
-    p1, p1o, p2,
-    pseudo_timer;
-    SDL_bool pr;
+typedef struct _Clock      Clock;
+typedef struct _MandalaPix MandalaPix;
+
+/* Clock struct */
+
+struct _Clock
+{
+    uint32_t last_tick;
+    uint32_t delta_ms;
 };
 
-void set_mandala_pix_values (mandala_pix* pix)
+Clock* Clock_Init()
 {
-    pix->x   = 120;
-    pix->y   = 240;
+    Clock* clock = malloc(sizeof(Clock));
 
-    pix->p1  = rand() % (21-10) + 10;             // between 10 and 20
-    pix->p1o = pix->p1;                           // origin value
-    pix->p2  = (double)rand() / (double)RAND_MAX; // between 0 and 0.999...
-    pix->pr  = SDL_FALSE;                         // reverse "projection"
+    clock->last_tick = 0;
+    clock->delta_ms  = 0;
 
-    pix->pseudo_timer = .0;
+    return clock;
 }
 
-mandala_pix* create_mandala_pix ()
+void Clock_Update(Clock* clock)
 {
-    mandala_pix* pix = malloc(sizeof(mandala_pix));
-    set_mandala_pix_values(pix);
+    uint32_t act_tick = SDL_GetTicks();
+    clock->delta_ms   = act_tick - clock->last_tick;
+    clock->last_tick  = act_tick;
+}
+
+/* MandalaPix struct */
+
+struct _MandalaPix
+{
+    float x, y;
+    float p1, p1o, p2;
+    float phase;
+};
+
+void MandalaPix_SetValues(MandalaPix* pix)
+{
+    pix->x = 120.f;
+    pix->y = 240.f;
+
+    pix->p1 = pix->p1o = rand() % (21-10) + 10;     // between 10 and 20 (p1o is p1 origin)
+    pix->p2 = -((float)rand()/(float)RAND_MAX);     // between 0 and -0.999...
+
+    pix->phase = 0.f;
+}
+
+MandalaPix* MandalaPix_Create()
+{
+    MandalaPix* pix = malloc(sizeof(MandalaPix));
+    MandalaPix_SetValues(pix);
     return pix;
 }
 
-/* FUNCTIONS FOR PROGRAM */
+/* Function for "general usage" */
 
-void set_black_screen(SDL_Renderer* ren)
+int CalculatePitch(uint32_t format, int width)
 {
-    SDL_SetRenderDrawColor(ren, (uint8_t) 0, (uint8_t) 0, (uint8_t) 0, (uint8_t) 255);
+    int pitch;
+
+    if (SDL_ISPIXELFORMAT_FOURCC(format) || SDL_BITSPERPIXEL(format) >= 8)
+         pitch = (width * SDL_BYTESPERPIXEL(format));
+    else pitch = ((width * SDL_BITSPERPIXEL(format)) + 7) / 8;
+
+    pitch = (pitch + 3) & ~3;
+
+    return pitch;
+}
+
+void GetRGBA (uint32_t const colour, uint8_t* const r, uint8_t* const g, uint8_t* const b, uint8_t* const a)
+{
+    *r = colour & 0xFF;
+    *g = (colour >> 8) & 0xFF;
+    *b = (colour >> 16) & 0xFF;
+    *a = colour >> 24;
+}
+
+void RenderClearToBlack(SDL_Renderer* ren)
+{
+    SDL_SetRenderDrawColor(ren, 0, 0, 0, 0);
     SDL_RenderClear(ren);
 }
 
@@ -53,121 +106,129 @@ uint8_t randColorByte()
     return rand() % 256;
 }
 
-/* Personnaly used for experimenting but not used in the final program.
+/* Main program */
 
-double fibonacci(int toDec)
+int main(int argc, char** argv)
 {
-    double U2,U1,U0;
-    double phi = 0.0;
-    U1 = 0; U0 = 1;
-
-    for (int i=0; i <= toDec; i++)
-    {
-        U2=U1+U0;
-        U0 =U1;
-        U1 =U2;
-    }
-
-    phi = U1/U0;
-
-    return phi;
-}
-
-*/
-
-/* PROGRAM */
-
-int main (int argc, char** argv)
-{
-    (void) argc; (void) argv; // Because unused
+    (void) argc; (void) argv;
 
     /* SDL window initialization */
 
-    if (SDL_Init (SDL_INIT_VIDEO) < 0) { printf("ERROR: %s", SDL_GetError()); return 1; };
+    if (SDL_Init(SDL_INIT_VIDEO) < 0)
+    {
+        fprintf(stderr, "ERROR: %s", SDL_GetError());
+        return -1;
+    };
 
-    SDL_Window* win = SDL_CreateWindow ("Mandala", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WIN_W, WIN_H, 0);
-    if (!win) { printf("ERROR: %s", SDL_GetError()); return 1; }
+    SDL_Window* win = SDL_CreateWindow("Mandala", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WIN_W, WIN_H, 0);
 
-    SDL_Renderer* ren = SDL_CreateRenderer (win, -1, SDL_RENDERER_ACCELERATED);
+    if (!win)
+    {
+        fprintf(stderr, "ERROR: %s", SDL_GetError());
+        return -1;
+    }
+
+    SDL_Renderer* ren = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED);
+
+    if (!ren)
+    {
+        fprintf(stderr, "ERROR: %s", SDL_GetError());
+        SDL_DestroyWindow(win); return -1;
+    }
+
     SDL_Event event;
 
     /* Program initialization */
 
-    unsigned int dt = 0,      // Ticks values is used for calculation of delta time
-                 a_tick = 0,
-                 b_tick = 0;
+    Clock* clock = Clock_Init();
 
-    srand(time(NULL)); // init for rand call
+    srand(time(NULL));
 
-    mandala_pix* pix = create_mandala_pix();
+    MandalaPix* pix = MandalaPix_Create();
+
+    const SDL_Rect screen_aera = { 0, 0, WIN_W, WIN_H };
+
+    uint32_t* pixels_data = malloc(sizeof(uint32_t[WIN_W * WIN_H])); // Will contain rendered content in order to redisplay it
+
+    const int pixels_pitch = CalculatePitch(SDL_PIXELFORMAT_RGBA32, WIN_W);
+
+    RenderClearToBlack(ren);
 
     SDL_bool running = SDL_TRUE;
-
-    set_black_screen(ren);
 
     /* Program execution */
 
     while (running)
     {
+        Clock_Update(clock);
 
         /* SDL event handling */
 
         while (SDL_PollEvent(&event))
         {
-
             running = event.type != SDL_QUIT; // Close program if requested
 
             if (event.type == SDL_MOUSEBUTTONDOWN) // Generate a new mandala
             {
-                set_mandala_pix_values(pix);
-                set_black_screen(ren);
+                MandalaPix_SetValues(pix);
+                RenderClearToBlack(ren);
             }
-
         }
 
-        /* Delta Time calculation and display according to it */
+        /* Update program */
 
-        a_tick = SDL_GetTicks();
-        dt = a_tick - b_tick;
+        pix->x += sin(pix->phase) * pix->p1;
+        pix->y += cos(pix->phase) * pix->p1;
 
-        if (dt>1000/60.0)
+        pix->p1 += pix->p2;
+        pix->phase += 0.1f;
+
+        if (pix->p1 < 0 || pix->p1 > pix->p1o)
         {
-            b_tick = a_tick;
-
-            /* Update program */
-
-            pix->x += sin(pix->pseudo_timer) * pix->p1;
-            pix->y += cos(pix->pseudo_timer) * pix->p1;
-
-            pix->pseudo_timer += .1;
-
-            if (pix->pr == SDL_FALSE)
-            {
-                if (pix->p1 > 0)
-                     pix->p1 -= pix->p2;
-                else pix->pr  = SDL_TRUE;
-            }
-            else if (pix->pr == SDL_TRUE)
-            {
-                if (pix->p1 < pix->p1o)
-                     pix->p1 += pix->p2;
-                else pix->pr  = SDL_FALSE;
-            }
-
-            /* Draw program */
-
-            SDL_SetRenderDrawColor(ren, randColorByte(), randColorByte(), randColorByte(), (uint8_t) 255);
-            SDL_RenderDrawPoint(ren, (int) pix->x, (int) pix->y);
-            SDL_RenderPresent(ren);
+            pix->p2 = -pix->p2;
         }
+
+        /* Draw pixel */
+
+        SDL_SetRenderDrawColor(ren, randColorByte(), randColorByte(), randColorByte(), 255);
+
+        SDL_RenderDrawPoint(ren, pix->x, pix->y);
+
+        /* Copy of the rendering to redisplay it after the next 'RenderClear', this avoids display glitches */
+
+        SDL_RenderReadPixels(ren, &screen_aera, SDL_PIXELFORMAT_RGBA32, pixels_data, pixels_pitch);
+
+        /* Render present / Render clear / Rewrite back render */
+
+        SDL_RenderPresent(ren);
+
+        RenderClearToBlack(ren);
+
+        for (int y = 0; y < WIN_H; y++) // This step is very slow
+        {
+            for (int x = 0; x < WIN_W; x++)
+            {
+                uint8_t r, g, b, a;
+                GetRGBA(pixels_data[y * WIN_W + x], &r, &g, &b, &a);
+                SDL_SetRenderDrawColor(ren, r, g, b, a);
+                SDL_RenderDrawPoint(ren, x, y); // The slowest
+            }
+        }
+
+        /* Limit CPU usage */
+
+        if (SDL_GetTicks() < (clock->last_tick + TPF))
+            SDL_Delay((clock->last_tick + TPF) - SDL_GetTicks());
     }
 
     /* Closing the program */
 
-    SDL_DestroyRenderer (ren);
-    SDL_DestroyWindow   (win);
-
+    free(pixels_data);
     free(pix);
+    free(clock);
+
+    SDL_DestroyRenderer(ren);
+    SDL_DestroyWindow(win);
     SDL_Quit();
 
     return 0;
